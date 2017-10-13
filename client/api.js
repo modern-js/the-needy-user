577070456b829a8ec0988f7d1c3c7498e09b69cd
Katiaras <giouzakovkostas@gmail.com>
2017-10-13 17:16:46 +0300

    lab session 2

--- client/api.js
+++ client/api.js
@@ -4,7 +4,23 @@ if (typeof global === 'undefined') {
 }
 
 /* write your code here */
-
+const everything = new Promise(
+  function(resolve, reject) {
+    resolve(global.fetch('/tv').then(
+      (a)=>{
+        return global.fetch('/drink').then(
+          (b)=>{
+              return global.fetch('/food').then(
+                (c)=>{
+                  return [a,b,c];
+                }
+              );
+          }
+        );
+      }
+    ));
+  }
+);
 
 
 // define the api-client object to be exported
@@ -22,9 +38,8 @@ api.get.me = function(something){
 api.get.me.a = function(something){
   return  global.fetch(something);
 }
-api.get.me.everything = function(){
-  return   Promise.resolve();
-}
+api.get.me.everything = () => everything;
+
 api.go.get = function (something){
   return  global.fetch(something);
 }
@@ -34,6 +49,7 @@ api.go.get.me= function (something){
 api.go.get.me.a = function(something){
   return  global.fetch(something);
 }
+api.go.get.me.everything = () => everything;
 
 // making the code work both in the browser and node.js
 if (typeof module !== 'undefined') { module.exports = api; }
